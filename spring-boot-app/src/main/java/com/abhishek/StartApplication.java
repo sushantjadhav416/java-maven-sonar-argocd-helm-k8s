d59e2ca832710b8f36ca00a60e6fc664b949dfd8
sushant jadhav <sushantjadhav416@gmail.com>
2024-04-15 13:27:20 +0530

    Added settings file

--- spring-boot-app/src/main/java/com/abhishek/StartApplication.java
+++ spring-boot-app/src/main/java/com/abhishek/StartApplication.java
@@ -11,7 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class StartApplication {
 
     @GetMapping("/")
-    public String index(final Model model) {
+    public String index(final Model model) 
+    {
         model.addAttribute("title", "I have successfuly built a sprint boot application using Maven");
         model.addAttribute("msg", "This application is deployed on to Kubernetes using Argo CD");
         return "index";
